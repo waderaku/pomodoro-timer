cbe419d1d232164adfc1014c391e7a9a009a9aec
waderaku <wadebosh@icloud.com>
2022-08-14 19:21:42 +0900

    urlの更新

--- backend/runtime/chalicelib/urls.py
+++ backend/runtime/chalicelib/urls.py
@@ -1,7 +1,19 @@
+from typing import Callable
+
 from chalice import Chalice
 
 from chalicelib.presentation.controller.authorize_controller import authorize
 
+from .presentation.controller.login_controller import login
+
+
+def route(app: Chalice, handler: Callable, path: str, methods: list[str]):
+    def wrapped(*args, **kwargs):
+        return handler(app, *args, **kwargs)
+
+    app.route(path, methods=methods)(wrapped)
+
 
 def app_routing(app: Chalice):
     app.authorizer()(authorize)
+    route(app, login, "/test", ["POST"])
