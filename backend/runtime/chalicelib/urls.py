6100d167afda2f7e7958284ed3383107a34fc62a
waderaku <wadebosh@icloud.com>
2022-08-14 22:17:20 +0900

    routing修正

--- backend/runtime/chalicelib/urls.py
+++ backend/runtime/chalicelib/urls.py
@@ -1,19 +1,27 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from chalice import Chalice
 
 from chalicelib.presentation.controller.authorize_controller import authorize
+from chalicelib.presentation.controller.register_event_controller import register_event
 
 from .presentation.controller.login_controller import login
 
 
-def route(app: Chalice, handler: Callable, path: str, methods: list[str]):
+def route(
+    app: Chalice,
+    handler: Callable,
+    path: str,
+    methods: list[str],
+    authorizer: Optional[Callable] = None,
+):
     def wrapped(*args, **kwargs):
-        return handler(app, *args, **kwargs)
+        return handler(app.current_request, *args, **kwargs)
 
-    app.route(path, methods=methods)(wrapped)
+    app.route(path, methods=methods, authorizer=authorizer)(wrapped)
 
 
 def app_routing(app: Chalice):
-    app.authorizer()(authorize)
-    route(app, login, "/test", ["POST"])
+    authorizer = app.authorizer()(authorize)
+    route(app, login, "/login", ["POST"])
+    route(app, register_event, "/event", ["POST"], authorizer)
