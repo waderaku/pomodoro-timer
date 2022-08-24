28482b8d9c168e5e80b22b24aadef6979f3ea4f5
waderaku <wadebosh@icloud.com>
2022-08-24 12:00:23 +0900

    DynamoDBデプロイ完了

--- backend/infrastructure/stacks/chaliceapp.py
+++ backend/infrastructure/stacks/chaliceapp.py
@@ -10,36 +10,49 @@ except ImportError:
 from chalice.cdk import Chalice
 
 RUNTIME_SOURCE_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), os.pardir, 'runtime')
+    os.path.dirname(os.path.dirname(__file__)), os.pardir, "runtime"
+)
 
 
 class ChaliceApp(cdk.Stack):
-
     def __init__(self, scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
-        # self.dynamodb_table = self._create_ddb_table()
+        self.dynamodb_table = self._create_ddb_table()
         self.chalice = Chalice(
-            self, 'ChaliceApp', source_dir=RUNTIME_SOURCE_DIR,
+            self,
+            "ChaliceApp",
+            source_dir=RUNTIME_SOURCE_DIR,
             stage_config={
-                'environment_variables': {
+                "environment_variables": {
                     # 'APP_TABLE_NAME': self.dynamodb_table.table_name,
                     "ENV": "prod"
                 }
-            }
+            },
         )
-        # self.dynamodb_table.grant_read_write_data(
-        #     self.chalice.get_role('DefaultRole')
-        # )
+        self.dynamodb_table.grant_read_write_data(self.chalice.get_role("DefaultRole"))
 
-    # def _create_ddb_table(self):
-    #     dynamodb_table = dynamodb.Table(
-    #         self, 'AppTable',
-    #         partition_key=dynamodb.Attribute(
-    #             name='PK', type=dynamodb.AttributeType.STRING),
-    #         sort_key=dynamodb.Attribute(
-    #             name='SK', type=dynamodb.AttributeType.STRING
-    #         ),
-    #         removal_policy=cdk.RemovalPolicy.DESTROY)
-    #     cdk.CfnOutput(self, 'AppTableName',
-    #                   value=dynamodb_table.table_name)
-    #     return dynamodb_table
+    def _create_ddb_table(self):
+        dynamodb_table = dynamodb.Table(
+            self,
+            "pomodoro_info",
+            table_name="pomodoro_info",
+            partition_key=dynamodb.Attribute(
+                name="ID", type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="DataType", type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PROVISIONED,
+            # 一旦読み書きのキャパシティを5ユニットに設定。随時調整
+            read_capacity=5,
+            write_capacity=5,
+        )
+        dynamodb_table.add_local_secondary_index(
+            sort_key=dynamodb.Attribute(
+                name="DataValue", type=dynamodb.AttributeType.STRING
+            ),
+            index_name="dataValueLSIndex",
+            projection_type=dynamodb.ProjectionType.ALL,
+        )
+        cdk.CfnOutput(self, "AppTableName", value=dynamodb_table.table_name)
+        return dynamodb_table
