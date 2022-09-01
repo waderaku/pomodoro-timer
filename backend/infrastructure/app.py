c33ee0e1ab77cdc80250e94c2a4c20b027ba40fe
satoshi.hieda <wadebosh@icloud.com>
2022-09-02 00:00:09 +0900

    s3 cdk作成

--- backend/infrastructure/app.py
+++ backend/infrastructure/app.py
@@ -5,8 +5,10 @@ except ImportError:
     import aws_cdk as cdk
 
 from stacks.chaliceapp import ChaliceApp
+from stacks.s3_bucket import S3Bucket
 
 app = cdk.App()
 ChaliceApp(app, "backend")
+S3Bucket(app, "frontend")
 
 app.synth()
