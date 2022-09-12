959ba5473f15a7debd9ac23597244d85f0a5e1c2
YoseMat <65691831+yotaro-shimose@users.noreply.github.com>
2022-09-12 22:43:35 +0900

    Revert "s3 cdk作成"

--- backend/infrastructure/app.py
+++ backend/infrastructure/app.py
@@ -5,10 +5,8 @@ except ImportError:
     import aws_cdk as cdk
 
 from stacks.chaliceapp import ChaliceApp
-from stacks.s3_bucket import S3Bucket
 
 app = cdk.App()
 ChaliceApp(app, "backend")
-S3Bucket(app, "frontend")
 
 app.synth()
