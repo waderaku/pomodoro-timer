f79237b85748ec08c0994be2e98443e98b570b55
yotaro.shimose <yosemat.beta@gmail.com>
2022-09-20 16:31:15 +0900

    タスクドメイン、タスクツリー作成

---
+++ backend/chalicelib/domain/model/collection/task_tree.py
@@ -0,0 +1,41 @@
+from backend.chalicelib.domain.exception.custom_exception import NoExistTaskException
+from backend.chalicelib.domain.model.entity.task import ROOT_TASK_ID, Task
+
+
+class TaskTree:
+    def __init__(self, task_list: list[Task]):
+        self.tree = {task.task_id: task for task in task_list}
+
+    def get_task(self, task_id: str) -> Task:
+        """タスクIDに紐づいたタスクを取得する
+
+        Args:
+            task_id (str): タスクID
+
+        Raises:
+            NoExistTaskException: 対象のタスクIDに紐づくタスクが存在しないことを示す例外
+
+        Returns:
+            Task: 取得したタスク情報
+        """
+        if task_id in self.tree:
+            self.tree.get(task_id)
+
+        raise NoExistTaskException()
+
+    def get_ancestor_list(self, task_id: str) -> list[Task]:
+        """タスクIDに紐づくタスクの親タスク一覧を取得する
+
+        Args:
+            task_id (str): タスクID
+
+        Returns:
+            list[Task]: 親タスク一覧
+        """
+        task = self.get_task(task_id)
+        ancestor_list = []
+        while task.parent_id != ROOT_TASK_ID:
+            task = self.get_task(task.parent_id)
+            ancestor_list.append(task)
+
+        return ancestor_list
--- backend/chalicelib/domain/model/entity/task.py
+++ backend/chalicelib/domain/model/entity/task.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from datetime import datetime
 
-
 ROOT_TASK_ID = "root"
 ROOT_TASK_NAME = "HOME"
 ROOT_TASK_WORKLOAD = 105120000
 
 
 class Task:
+    """タスクドメイン"""
+
     def __init__(
         self,
         user_id: str,
@@ -16,6 +17,8 @@ class Task:
         name: str,
         shortcut_flg: bool,
         children_task_id: list[str],
+        parent_id: str,
+        event_id_list: list[str],
         done: bool,
         finished_workload: int,
         estimated_workload: int,
@@ -27,14 +30,72 @@ class Task:
         self._name = name
         self._shortcut_flg = shortcut_flg
         self._children_task_id = children_task_id
+        self._parent_id = parent_id
+        self._event_id_list = event_id_list
         self._done = done
         self._finished_workload = finished_workload
         self._estimated_workload = estimated_workload
         self._deadline = deadline
         self._notes = notes
 
+    @property
+    def user_id(self) -> str:
+        return self._user_id
+
+    @property
+    def task_id(self) -> str:
+        return self._task_id
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def shortcut_flg(self) -> bool:
+        return self._shortcut_flg
+
+    @property
+    def children_task_id(self) -> list[str]:
+        return self._children_task_id
+
+    @property
+    def parent_id(self) -> str:
+        return self._parent_id
+
+    @property
+    def event_id_list(self) -> list[str]:
+        return self._event_id_list
+
+    @property
+    def done(self) -> bool:
+        return self._done
+
+    @property
+    def finished_workload(self) -> int:
+        return self._finished_workload
+
+    @property
+    def estimated_workload(self) -> int:
+        return self._estimated_workload
+
+    @property
+    def deadline(self) -> datetime:
+        return self._deadline
+
+    @property
+    def notes(self) -> str:
+        return self._notes
+
     @classmethod
     def create_root(cls, user_id: str) -> Task:
+        """ルートタスクのドメインオブジェクトを作成するファクトリ関数
+
+        Args:
+            user_id (str): ユーザID
+
+        Returns:
+            Task: 作成したルートタスクのドメイン
+        """
         return cls(
             user_id=user_id,
             task_id=ROOT_TASK_ID,
@@ -42,6 +103,8 @@ class Task:
             shortcut_flg=False,
             children_task_id=[],
             done=False,
+            parent_id="",
+            _event_id_list=[],
             finished_workload=ROOT_TASK_WORKLOAD,
             estimated_workload=ROOT_TASK_WORKLOAD,
             deadline="2200-12-31",
