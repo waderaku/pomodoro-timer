2dbb1b689cf27efee22452373b2221ef48530254
yosemat.beta@gmail.com <yosemat.beta@gmail.com>
2022-09-21 23:57:19 +0900

    taskを統一し、データクラスを採用（これまではデータクラスのものとそうでないものが散らばっていた）

--- backend/chalicelib/domain/model/entity/task.py
+++ backend/chalicelib/domain/model/entity/task.py
@@ -1,43 +1,34 @@
 from __future__ import annotations
 
+from dataclasses import dataclass, replace
 from datetime import datetime
-
+from typing import Any, Optional
 
 ROOT_TASK_ID = "root"
 ROOT_TASK_NAME = "HOME"
 ROOT_TASK_WORKLOAD = 105120000
 
 
+@dataclass
 class Task:
-    def __init__(
-        self,
-        user_id: str,
-        task_id: str,
-        name: str,
-        shortcut_flg: bool,
-        children_task_id: list[str],
-        done: bool,
-        finished_workload: int,
-        estimated_workload: int,
-        deadline: datetime,
-        notes: str,
-    ):
-        self._user_id = user_id
-        self._task_id = task_id
-        self._name = name
-        self._shortcut_flg = shortcut_flg
-        self._children_task_id = children_task_id
-        self._done = done
-        self._finished_workload = finished_workload
-        self._estimated_workload = estimated_workload
-        self._deadline = deadline
-        self._notes = notes
+    user_id: str
+    task_id: str
+    parent_id: Optional[str]
+    name: str
+    shortcut_flg: bool
+    children_task_id: list[str]
+    done: bool
+    finished_workload: int
+    estimated_workload: int
+    deadline: datetime
+    notes: str
 
     @classmethod
     def create_root(cls, user_id: str) -> Task:
         return cls(
             user_id=user_id,
             task_id=ROOT_TASK_ID,
+            parent_id=None,
             name=ROOT_TASK_NAME,
             shortcut_flg=False,
             children_task_id=[],
@@ -47,3 +38,18 @@ class Task:
             deadline="2200-12-31",
             notes="",
         )
+
+    def update(
+        self,
+        /,
+        **changes: dict[str:Any],
+    ) -> Task:
+        """指定されたフィールドが更新された'新しい'インスタンスをリターンする
+
+        Returns:
+            Task: 特定のフィールドが更新された新インスタンス
+        """
+        return replace(self, **changes)
+
+    def is_root(self) -> bool:
+        return self.parent_id is None
