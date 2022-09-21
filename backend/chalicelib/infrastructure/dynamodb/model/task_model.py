3a0fb9745075528c7f40786d166c6b2c9ce61e01
yosemat.beta@gmail.com <yosemat.beta@gmail.com>
2022-09-22 00:04:09 +0900

    多少リネーム。旧TaskModel（FastAPIのモデルと重複していた）にドメインモデルやDynamoDBへの変換ロジックを実装。そしてtask_dynamo_repositoryを実装。

--- backend/chalicelib/infrastructure/dynamodb/model/task_model.py
+++ backend/chalicelib/infrastructure/dynamodb/model/task_model.py
@@ -1,47 +1,78 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+from decimal import ROUND_HALF_UP, Decimal
 
 from chalicelib.domain.model.entity.task import Task
 
 
 @dataclass
-class TaskInfoModel:
+class TaskInfoDynamoModel:
     name: str
     shortcut_flg: bool
+    parent_id: str
     children_task_id: list[str]
     finished_workload: int
     estimated_workload: int
     deadline: str
     notes: str
 
+    def __post_init__(self):
+        self.finished_workload = self._quantize(self.finished_workload)
+        self.estimated_workload = self._quantize(self.estimated_workload)
+
+    def _quantize(self, float_variable: float) -> float:
+        return float(
+            Decimal(str(float_variable)).quantize(
+                Decimal("0.1"), rounding=ROUND_HALF_UP
+            )
+        )
+
 
 @dataclass
-class TaskModel:
+class TaskDynamoModel:
     ID: str
     DataType: str
     DataValue: str
-    TaskInfo: TaskInfoModel
+    TaskInfo: TaskInfoDynamoModel
 
     @classmethod
-    def to_model(cls, task: Task) -> TaskModel:
+    def from_task(cls, task: Task) -> TaskDynamoModel:
         """ドメインのユーザオブジェクトをDBに登録する形式に変換する
 
         Args:
             task (Task): ユーザオブジェクト
         """
-        task_info_model = TaskInfoModel(
-            name=task._name,
-            shortcut_flg=task._shortcut_flg,
-            children_task_id=task._children_task_id,
-            finished_workload=task._finished_workload,
-            estimated_workload=task._estimated_workload,
-            deadline=task._deadline,
-            notes=task._notes,
+        task_info_model = TaskInfoDynamoModel(
+            name=task.name,
+            shortcut_flg=task.shortcut_flg,
+            children_task_id=task.children_task_id,
+            finished_workload=task.finished_workload,
+            estimated_workload=task.estimated_workload,
+            deadline=task.deadline,
+            notes=task.notes,
         )
         return cls(
-            ID=f"{task._user_id}_task",
-            DataType=task._task_id,
-            DataValue="True" if task._done else "False",
+            ID=f"{task.user_id}_task",
+            DataType=task.task_id,
+            DataValue="True" if task.done else "False",
             TaskInfo=task_info_model,
         )
+
+    # TODO CDKでTable定義にparentId加える
+
+    def to_task(self) -> Task:
+        return Task(
+            user_id=self.ID.split("_")[0],
+            task_id=self.DataType,
+            parent_id=self.TaskInfo.parent_id,
+            name=self.TaskInfo.name,
+            shortcut_flg=self.TaskInfo.shortcut_flg,
+            children_task_id=self.TaskInfo.children_task_id,
+            finished_workload=self.TaskInfo.finished_workload,
+            estimated_workload=self.TaskInfo.estimated_workload,
+            deadline=self.TaskInfo.deadline,
+        )
+
+    def to_dynamo_input(self) -> dict:
+        return asdict(self)
--- backend/chalicelib/infrastructure/dynamodb/repository/task_user_dynamo_repository.py
+++ backend/chalicelib/infrastructure/dynamodb/repository/task_user_dynamo_repository.py
@@ -2,16 +2,18 @@ from dataclasses import asdict
 
 from chalicelib.domain.model.entity.task_user import TaskUser
 from chalicelib.domain.repository.task_user_repository import TaskUserRepository
-from chalicelib.infrastructure.dynamodb.model.task_model import TaskModel
+from chalicelib.infrastructure.dynamodb.model.task_model import TaskDynamoModel
 from chalicelib.infrastructure.dynamodb.model.user_model import UserModel
-from chalicelib.infrastructure.dynamodb.repository.dynamo_repository import DynamoRepository
+from chalicelib.infrastructure.dynamodb.repository.dynamo_repository import (
+    DynamoRepository,
+)
 
 
 class TaskUserDynamoRepository(TaskUserRepository, DynamoRepository):
     def register_task_user(self, task_user: TaskUser):
         user_model = UserModel.to_model(task_user._user)
         task_model_list = [
-            TaskModel.to_model(task=task) for task in task_user._task_list
+            TaskDynamoModel.from_task(task=task) for task in task_user._task_list
         ]
         with self._table.batch_writer() as batch:
             batch.put_item(Item=asdict(user_model))
